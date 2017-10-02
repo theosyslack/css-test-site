c7a8e3d88fd1ff3fd4831a53a8c9064412a3346f
Theo Syslack <theosyslack@gmail.com>
2017-10-01 20:21:06 -0500

    Added some JS for sticky header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './css/main.css';
+window.addEventListener('load', function(){
+    document.body.classList.remove('preload');
+});
+window.addEventListener('scroll', function(e){
+    let header = document.querySelector('.Header');
+    let body = document.querySelector('.Body');
+    let headerBoundaries = header.getBoundingClientRect();
+    
+    if (headerBoundaries.top === -1){
+        header.classList.add('Header--is-sticky');
+        body.classList.add('Body--dark-mode');
+        
+    } else {
+        header.classList.remove('Header--is-sticky');
+        body.classList.remove('Body--dark-mode');
+    }
+})
