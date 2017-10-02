e6faf82e480f0b4bb40bc1b104e1801c624b3c0a
Theo Syslack <theosyslack@gmail.com>
2017-10-01 20:28:45 -0500

    Added less jarring "reading mode"

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import './css/main.css';
+
+
 window.addEventListener('load', function(){
     document.body.classList.remove('preload');
-});
-window.addEventListener('scroll', function(e){
     let header = document.querySelector('.Header');
     let body = document.querySelector('.Body');
-    let headerBoundaries = header.getBoundingClientRect();
     
-    if (headerBoundaries.top === -1){
-        header.classList.add('Header--is-sticky');
-        body.classList.add('Body--dark-mode');
-        
-    } else {
-        header.classList.remove('Header--is-sticky');
-        body.classList.remove('Body--dark-mode');
-    }
-})
+    window.addEventListener('scroll', function(e){
+        let headerBoundaries = header.getBoundingClientRect();
+        console.log('scroll')
+        if (headerBoundaries.top === -1){
+            header.classList.add('Header--is-sticky');
+            body.classList.add('Body--dark-mode');
+            
+        } else {
+            header.classList.remove('Header--is-sticky');
+            body.classList.remove('Body--dark-mode');
+        }
+    })
+});
